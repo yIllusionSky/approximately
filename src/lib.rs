40f544d7eb1c02de65d1e5e3afb3d00396b5a6e8
YaoLinQing <d161442079@163.com>
2024-07-31 23:37:57 +0800

    add(smid,option,[])

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,15 @@
 //!
 //! ApproxEq is used to provide an approximate equality for the type you want, but your type must implement the approx trait itself.
 //! By default, [`f32`] and [`f64`] have an implementation with tolerances of `1e-3` and `1e-6`, respectively.
-#![deny(broken_intra_doc_links)]
+#![deny(rustdoc::broken_intra_doc_links)]
 #![warn(missing_docs)]
 #![warn(clippy::all, clippy::nursery)]
-
+#![feature(cfg_match)]
+#![feature(portable_simd)]
 use std::{borrow::Borrow, fmt::Debug};
+
+#[cfg(feature = "simd")]
+use std::simd::{f32x4, f64x4, num::SimdFloat};
 
 /// Evaluate if the values.
 #[allow(dead_code)]
@@ -37,11 +41,62 @@ impl ApproxEq for f64 {
     }
 }
 
+impl<A> ApproxEq for [A]
+where
+    A: ApproxEq + Debug + Clone,
+{
+    fn approx<T: Borrow<Self>>(&self, other: T) -> bool {
+        if self.len() != other.borrow().len() {
+            return false;
+        }
+        self.iter()
+            .zip(other.borrow().iter())
+            .all(|(a, b)| a.approx(b))
+    }
+    fn assert_approx<T: Borrow<Self> + Debug + Clone>(&self, other: T) {
+        assert!(self.approx(other.clone()), "{self:?} != {other:?}");
+    }
+}
+
+impl<A> ApproxEq for Option<A>
+where
+    A: ApproxEq + Debug + Clone,
+{
+    fn approx<T: Borrow<Self>>(&self, other: T) -> bool {
+        match (self, other.borrow()) {
+            (Some(a), Some(b)) => a.approx(b),
+            (None, None) => true,
+            _ => false,
+        }
+    }
+    fn assert_approx<T: Borrow<Self> + Debug + Clone>(&self, other: T) {
+        assert!(self.approx(other.clone()), "{self:?} != {other:?}");
+    }
+}
+
+#[cfg(feature = "simd")]
+impl ApproxEq for f32x4 {
+    fn approx<T: Borrow<Self>>(&self, other: T) -> bool {
+        (*self - other.borrow()).abs() <= Self::splat(1e-3)
+    }
+    fn assert_approx<T: Borrow<Self> + Debug + Clone>(&self, other: T) {
+        assert!(self.approx(other.clone()), "{self:?} != {other:?}");
+    }
+}
+
+#[cfg(feature = "simd")]
+impl ApproxEq for f64x4 {
+    fn approx<T: Borrow<Self>>(&self, other: T) -> bool {
+        (*self - other.borrow()).abs() <= Self::splat(1e-6)
+    }
+    fn assert_approx<T: Borrow<Self> + Debug + Clone>(&self, other: T) {
+        assert!(self.approx(other.clone()), "{self:?} != {other:?}");
+    }
+}
 #[cfg(test)]
 mod tests {
+    use super::*;
     use std::ops::Not;
-
-    use crate::ApproxEq;
 
     #[test]
     fn test_approx_eq() {
@@ -59,12 +114,20 @@ mod tests {
     fn test_assert_approx_eq() {
         1.0000f32.assert_approx(1.0001f32);
         1.000000f64.assert_approx(1.000001f64);
+
+        #[cfg(feature = "simd")]
+        {
+            f32x4::splat(1.0000).assert_approx(f32x4::splat(1.0001));
+            f64x4::splat(1.000000).assert_approx(f64x4::splat(1.000001));
+        }
     }
 
     #[test]
     #[should_panic]
     fn test_assert_not_approx_eq() {
         1.000f32.assert_approx(1.001f32);
+
+        // this is not work
         1.0000f64.assert_approx(1.00001f64);
     }
 }
